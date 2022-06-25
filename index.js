d11e5f4540d3ee42659a79ae07a42fbca761a9f6
Ahmed-Osama-Salem <97908950+Ahmed-Osama-Salem@users.noreply.github.com>
2022-06-25 16:32:17 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -246,7 +246,7 @@ if (navigator.geolocation)
       const longitude = position.coords.longitude;
       console.log(latitude, longitude);
       const myPosition = [latitude, longitude];
-      const map = L.map('map').setView(myPosition, 14);
+      const map = L.map('map').setView(myPosition, 18);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
